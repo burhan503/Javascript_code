5fb48baa87496cdca90705e5c7d347c403f48b3f
Burhan <91361396+burhan503@users.noreply.github.com>
2024-01-24 07:08:17 +0000

    classes & Inheritance new Example Commit

--- JavaScript ES6/classes_&_inheritance.js
+++ JavaScript ES6/classes_&_inheritance.js
@@ -1,30 +1,31 @@
-class Animal {
-    constructor(name, sound) {
+//Simple example of using classes and Inheritance in JavaScript
+class Animal{
+    constructor(name,sound){
         this.name = name;
         this.sound = sound;
     }
-    makeSound() {
-        console.log(`${this.sound} this is the sound  of this ${this.name}animal`);
+    makeSound(){
+        console.log(` this is the name ${this.name} & this is the Sound ${this.sound}`);
     }
 }
-// Child class inherit from Animal
-class Dog extends Animal {
-    constructor(name, breed) {
-        // call the constructor the parent class is Animal
-        super(name, 'Wolf');
-        this.breed = breed;
+// child class inherit from Animal
+class Dog extends Animal{
+    constructor(name, breed){
+     // ca;; the constructor the parent class is Animal
+     super(name,' WOLF');
+     this.breed= breed;
     }
-    // Override the makeSound method of the parent class
-    makeSound() {
-        console.log(`${this.name} barks: ${this.sound}`);
+    // Overrite the MakeSound method of the Parent class 
+    makeSound(){
+        console.log(`this is the name ${this.name} & this is the Sound ${this.sound}`);
     }
-    fetch() {
-        console.log(`${this.name} is fetching the ball!`);
-
+    fetch(){
+        console.log(`this is the name ${this.name} is fetching the ball ~~`);
     }
 }
 // create instance of classes 
-const makemysound = new Animal('Pink Persian cat', 'Maaow Maaow');
-const myDog = new Dog('buddy','Golden Retriever');
-makemysound.makeSound();
-myDog.makeSound();
+const make_Sound_Key = new Animal('Black Persion Cat',' Maaw');
+const MyDog = new Dog(' Buddy',' Golden Retriever');
+make_Sound_Key.makeSound();
+MyDog.makeSound();
+console.log('All Set');
