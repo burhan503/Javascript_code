b3b8c522b31296cb648cbbd19e15019361345939
Burhan <91361396+burhan503@users.noreply.github.com>
2024-01-25 08:52:39 +0000

    Add Code PUSH & POP

---
+++ JavaScript ES6/JavaScript_Array_Method.js
@@ -0,0 +1,11 @@
+// JavaScript Array methods with example 
+
+// 1 PUSH(): Add or more data or element to the end of an Array;
+let food = ['Pizza','Dosa','Dal','Palak'];
+console.log(food);
+food.push('Dal Bati');
+console.log(food);
+// 2 Remove Last Element on Array Method is POP
+food.pop();
+console.table(food);
+console.log('All Set');
