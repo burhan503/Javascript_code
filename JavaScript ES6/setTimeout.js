b74588a169b8d5b4eb0b261fbef4d95ad4258216
Burhan <91361396+burhan503@users.noreply.github.com>
2024-01-27 23:41:47 +0530

    Update With New Example setTimeout.js

--- JavaScript ES6/setTimeout.js
+++ JavaScript ES6/setTimeout.js
@@ -4,5 +4,27 @@
 function greet(){
     console.log('Hello Developers')
 }
-setTimeout(greet, 3000);
+setTimeout(greet, 2000);
 console.log('display first time console');
+
+// second example of setTimeout 
+
+function great(){
+    let name = "Burhan Khan"
+    let age = 27;
+    console.log(`What is Your Name ${name} & Age is ${age}`);
+    console.log(`Example second here 2`);
+}
+let interval_id = setTimeout(great,5000);
+//console.log('Your Interval ID Here',interval_id);
+
+function ShowTime(){
+    let NewDate = new Date();
+    let getLocalDate = NewDate.toLocaleTimeString();
+    console.log(getLocalDate);
+    const set_time = setTimeout(ShowTime, 6000);
+    console.log(set_time);
+    clearTimeout(set_time);
+}
+ShowTime();
+
